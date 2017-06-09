a1dd87ad4925ea54dce874149dd0d0e3a122d019
steven2805 <stevenmac90@googlemail.com>
2017-06-09 19:07:19 +0100

    added tickets

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,20 @@ require_relative('../ticket')
 require ('pry-byebug')
 
 customer1 = Customer.new({ 'name' => 'Steven'})
+customer2 = Customer.new({ 'name' => 'peter'})
 customer1.save()
+customer2.save()
 
 movie1 = Movie.new({ 'title' => 'Transporter', 'price' => "20"})
-
+movie2 = Movie.new({'title' => 'ghostbusters', 'price' => '15'})
 movie1.save()
+movie2.save()
+
+ticket1 = Ticket.new({ 'customer_id' => customer1.id, 'movie_id' => movie1.id})
+ticket2 = Ticket.new({ 'customer_id' => customer2.id, 'movie_id' => movie2.id})
+ticket1.save()
+ticket2.save()
+
 
 
 
